f735435224ee3926077a59aa090453117be51bd4
Chris Mailloux <chrismayu@gmail.com>
2013-09-11 07:42:25 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BenGeocachingApp::Application.config.secret_token = '55f55cb6b1125a65b89cbd511b005626f60e69d53758bee860f3828606019468b3a88b120d49a38406747b5afeaefa5790e848a6155e24246f9bf4e78678ed54'
