dd3cd8cbb4b8ceacc3589ea75b4a28cd9eadf5ea
Chris Mailloux <chrismayu@gmail.com>
2013-09-12 21:20:23 -0400

    footer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ BenGeocachingApp::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false #true
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
